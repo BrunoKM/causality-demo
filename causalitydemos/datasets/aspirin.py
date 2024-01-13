9aa69db6bbfd2a16cd6016312136476066b2fb1b
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2024-01-13 09:08:15 +0000

    add notebooks and update

--- causalitydemos/datasets/aspirin.py
+++ causalitydemos/datasets/aspirin.py
@@ -50,3 +50,34 @@ class LognormalAspirinModel:
         t = np.ones([num_examples]) * dose
         y = self._sample_y_cond_on_zt(z=z, t=t)
         return z, t, y 
+
+    def calc_exp_y_cf_given_ty(self, t, y, t_cf):
+        return calc_exp_y_cf_given_ty(
+            t, y, t_cf, self.a, self.b, self.c, self.severity_mean,
+            self.severity_sigma, self.dosage_sigma, self.duration_sigma)
+
+
+def calc_exp_y_cf_given_ty(t, y, t_cf, a, b, c, severity_mean, severity_sigma, dosage_sigma, duration_sigma):
+    d = b - a * c
+    # The covariance matrix for the variables T, Y
+    cond_cov_submatrix = np.array(
+        [[a**2 * severity_sigma**2 + dosage_sigma**2, a*d*severity_sigma**2-c*dosage_sigma**2],
+         [a*d*severity_sigma**2-c*dosage_sigma**2, d**2*severity_sigma**2 + c**2*dosage_sigma**2 + duration_sigma**2]])
+    # cross-covariance term between Y_cf and T, Y
+    cross_cov_submatrix = np.array(
+        [[a*b*severity_sigma**2], [b*d*severity_sigma**2]]
+    )
+    mu_y_cf = (b*severity_mean - c*np.log(t_cf)) 
+    mu_ty = np.array([a, d])*severity_mean
+    cond_cov_submatrix_inv = np.linalg.inv(cond_cov_submatrix)
+    # Calculate the log-normal gaussian mu parameter (not same as mean)
+    mu_y_cf_given_ty = mu_y_cf + np.matmul(
+        cross_cov_submatrix.T, np.matmul(cond_cov_submatrix_inv, (np.log(np.array([t, y])) - mu_ty)))
+    # Calculate the covariance
+    sigma_squared_y_cf_given_ty = (b**2*severity_sigma**2 + duration_sigma**2) - np.matmul(
+        cross_cov_submatrix.T, np.matmul(cond_cov_submatrix_inv, cross_cov_submatrix))
+    # Calculate the mean of the lognormal variable
+    return np.exp(mu_y_cf_given_ty + sigma_squared_y_cf_given_ty / 2)
+    
+    
+    
